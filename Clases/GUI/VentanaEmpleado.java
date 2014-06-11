8a70911c669214c6e7cb2e3064e2e6319d83ce1c
felipetibatag <luisfelipetibatag@gmail.com>
2014-06-11 18:31:31 -0500

    6:31 última modificación

--- Clases/GUI/VentanaEmpleado.java
+++ Clases/GUI/VentanaEmpleado.java
@@ -12,10 +12,11 @@ import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
 public class VentanaEmpleado extends JFrame{
-	JLabel lnombre=new JLabel("Nombre");
-	JLabel cedula=new JLabel("Cedula");
+	JLabel lNombre=new JLabel("Nombre");
+	JLabel lCedula=new JLabel("Cedula");
 	JLabel lTitulo=new JLabel("Registro Empleado");
-	JTextField Tnombre=new JTextField(10);
+	JTextField tNombre=new JTextField(10);
+	JTextField tCedula=new JTextField(10);
 	JButton bGuardar=new JButton("Guardar");
 	JButton bImprimir=new JButton("Imprimir");
 	private GridBagConstraints gbc=new GridBagConstraints();
@@ -30,8 +31,8 @@ public class VentanaEmpleado extends JFrame{
 		gbc.gridheight=1;
 		gbc.gridwidth=4;
 		gbc.weightx=1.0;
-		gbc.weighty=1.0;
-		//gbc.fill=GridBagConstraints.HORIZONTAL;
+		gbc.weighty=0.0;
+		gbc.fill=GridBagConstraints.NONE;
 		gbc.anchor=GridBagConstraints.NORTH;
 		gbc.insets=new Insets(10, 5, 5, 5);
 		contenedor.add(lTitulo,gbc);
@@ -40,20 +41,38 @@ public class VentanaEmpleado extends JFrame{
 		gbc.gridy=1;
 		gbc.gridheight=1;
 		gbc.gridwidth=1;
+		gbc.weightx=0.0;
+		gbc.weighty=0.0;
+		gbc.anchor=GridBagConstraints.WEST;
+		gbc.fill=GridBagConstraints.HORIZONTAL;
+		contenedor.add(lNombre,gbc);
+		
+		gbc.gridx=1;
+		gbc.gridy=1;
+		gbc.gridheight=1;
+		gbc.gridwidth=GridBagConstraints.RELATIVE;
 		gbc.weightx=1.0;
 		gbc.weighty=0.0;
-		//gbc.anchor=GridBagConstraints.WEST;
 		gbc.fill=GridBagConstraints.HORIZONTAL;
-		contenedor.add(lnombre,gbc);
+		contenedor.add(tNombre,gbc);
 		
-		gbc.gridx=GridBagConstraints.RELATIVE;
-		gbc.gridy=1;
-		gbc.gridheight=2;
+		gbc.gridx=0;
+		gbc.gridy=2;
+		gbc.gridheight=1;
+		gbc.gridwidth=1;
+		gbc.weightx=0.0;
+		gbc.weighty=0.0;
+		gbc.fill=GridBagConstraints.HORIZONTAL;
+		contenedor.add(lCedula,gbc);
+		
+		gbc.gridx=1;
+		gbc.gridy=2;
+		gbc.gridheight=1;
 		gbc.gridwidth=1;
 		gbc.weightx=1.0;
 		gbc.weighty=0.0;
 		gbc.fill=GridBagConstraints.HORIZONTAL;
-		contenedor.add(Tnombre,gbc);
+		contenedor.add(tCedula,gbc);
 		
 		
 		
